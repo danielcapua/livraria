6f8aad9a543f010a1da471988d82a7dd9a787d77
danielcapua <danielcapua@yahoo.com.br>
2014-08-11 09:35:03 -0300

    views

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -3,7 +3,13 @@ class Home extends CI_Controller {
     public function __construct() {
         parent::__construct();
     }
-    public function index( ){
-        $this->load-> view('home');
+    public function index( )
+    {
+        $this->load-> view('html_header');
+        $this->load-> view('header');
+        $this->load-> view('menu_categorias');
+        $this->load-> view('conteudo');
+        $this->load-> view('rodape');
+        $this->load-> view ('html_footer');
     }
 }
